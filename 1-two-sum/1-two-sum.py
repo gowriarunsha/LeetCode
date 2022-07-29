29b03c3d62594c1a5a3bf0b2f65f5f42c7281865
Gowri Arunsha <59527493+gowriarunsha@users.noreply.github.com>
2022-07-29 21:55:46 +0530

    Time: 4643 ms (17.40%), Space: 14.5 MB (22.19%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -9,6 +9,5 @@ class Solution(object):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
-                    l.append(i)
-                    l.append(j)
+                    l+=[i,j]
         return l
